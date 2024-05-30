1a9bac483dceed94672ad12629f3e0d492217fe5
Beater420 <caliaro.alexandre@gmail.com>
2024-05-30 13:57:11 +0200

    Implement game logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import com.beater.game.Hangman;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Hangman hangman = new Hangman();
@@ -7,5 +9,51 @@ public class Main {
                 "You have a limited number of attempts, so choose wisely. Good luck!"
         );
         System.out.println(hangman.getGuessWord());
+
+        while (true) {
+            if (!hangman.getGuessedLetter().isEmpty()) {
+                System.out.println("Letters already tried: " + hangman.getGuessedLetter());
+            }
+
+            char letter = scanLetter();
+            if (!hangman.guessLetter(letter)) {
+                System.out.println("Aie Aie Aie... " + hangman.getLife() + " attempts left");
+            } else {
+                System.out.println("Great job! You found a letter !");
+                System.out.println(hangman.getGuessWord());
+            }
+
+            if (hangman.isLose()) {
+                System.out.println("Better luck next time! The word was " + hangman.getSecretWord());
+                break;
+            }
+
+            if (hangman.isWon()) {
+                System.out.println("Awesome! You figured out the word " + hangman.getSecretWord() +
+                        ". You're Rock !"
+                );
+                break;
+            }
+        }
     }
+
+    /**
+     * Method which loops until the player enters a unique letter.
+     *
+     * @return letter enter by the player
+     */
+    private static char scanLetter() {
+        final var scanner = new Scanner(System.in);
+        String letter;
+        do {
+            System.out.println("Enter a letter:");
+            letter = scanner.nextLine();
+            if (letter.length() > 1) {
+                System.out.println("You should enter only one letter:");
+            }
+        } while (letter.length() != 1);
+
+        return letter.charAt(0);
+    }
+}
 }
--- src/com/beater/game/Hangman.java
+++ src/com/beater/game/Hangman.java
@@ -1,6 +1,7 @@
 package com.beater.game;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
@@ -15,6 +16,16 @@ public class Hangman {
      * Stores letters discovered by the player. '-' stored for not discovered letters.
      */
     private final List<Character> guessWord = new ArrayList<>();
+
+    /**
+     * Stores letters that the player has used to try to discover the secret word.
+     */
+    private final HashSet<Character> guessedLetter = new HashSet<>();
+
+    /**
+     * Stores the remaining number of life points.
+     */
+    private int life = 10;
 
     /**
      * Build a Hangman object.
@@ -36,11 +47,64 @@ public class Hangman {
         }
     }
 
-    public List<Character> getGuessWord() {
-        return guessWord;
+    /**
+     * Method which verifies if a char given by the player is discovered in the secret word.
+     * @param letter The letter to validate in `secretWord` and `guessWord`.
+     */
+    public boolean guessLetter(char letter) {
+        guessedLetter.add(letter);
+        boolean asLetter = secretWord.contains(String.valueOf(letter)) && !guessWord.contains(letter);
+        if (asLetter) {
+            int index = secretWord.indexOf(letter);
+            while (index >= 0) {
+                this.guessWord.set(index, letter);
+                index = secretWord.indexOf(letter, index + 1);
+            }
+        } else {
+            life--;
+        }
+
+        return asLetter;
+    }
+
+    /**
+     * Check if the game is won.
+     * @return boolean true is the game is won, false otherwise.
+     */
+    public boolean isWon() {
+        return !guessWord.contains('-');
+    }
+
+
+    /**
+     * Check if the game is lost.
+     * @return boolean true if the game is lost, false otherwise.
+     */
+    public boolean isLose() {
+        return life <= 0;
     }
 
     public String getSecretWord() {
         return secretWord;
+    }
+
+    public List<Character> getGuessWord() {
+        return guessWord;
+    }
+
+    public HashSet<Character> getGuessedLetter() {
+        return guessedLetter;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    @Override
+    public String toString() {
+        return "Hangman{" +
+                "life=" + life +
+                ", guessWord=" + guessWord +
+                '}';
     }
 }
